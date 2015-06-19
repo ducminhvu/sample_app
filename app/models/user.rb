ea9ec53635de1ef6f43f3d5f727ae4ebe13ad24c
ducminhvu <vu.minh.duc@framgia.com>
2015-06-19 23:49:54 +0700

    edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token
-
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -27,9 +26,10 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-    # Returns true if the given token matches the digest.
+  # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
 
 end
