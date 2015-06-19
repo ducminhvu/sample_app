8afdf8ecd9a74cbd3d6b04cc941fc2b35bcd713b
ducminhvu <vu.minh.duc@framgia.com>
2015-06-19 22:55:49 +0700

    cookie create

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "contact"   => "static_pages#contact"
   get "signup"    => "users#new"
   
+  resources :users
   get "login"     => "sessions#new"
   post "login"    => "sessions#create"
   delete "logout" => "sessions#destroy"
