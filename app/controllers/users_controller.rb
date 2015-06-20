c169f03f8bdc5a1a3093ab2555a0c9034b6c35f8
ducminhvu <vu.minh.duc@framgia.com>
2015-06-20 19:14:21 +0700

    edit something chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page], :per_page => 5)
+  end
+
   def new
     @user = User.new
   end
@@ -9,13 +16,27 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      log_in user
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to user
+      redirect_to @user
     else
       render "new"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Update success!"
+      redirect_to @user
+    else
+      render "edit"
     end
   end
 
@@ -23,5 +44,22 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                     :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+      # if !logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    def correct_user
+      if current_user.id.to_s != params[:id]
+        flash[:danger] = "You can't edit other user profile."
+        redirect_to root_path
+      end
+      # @user = User.find(params[:id])
+      # redirect_to(root_url) unless current_user?(@user)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-admin = User.create(name: "Vu Minh Duc", email: "dev.ducvu@gmail.com", 
-                    password: "abc123", password_confirmation: "abc123")
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar", admin: true)
+
+User.create(name: "Vu Minh Duc", email: "dev.ducvu@gmail.com", 
+                    password: "abc123", password_confirmation: "abc123", admin: true)
+40.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
