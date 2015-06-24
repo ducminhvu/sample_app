59f4b58a42f69fac97426486f8ddb1003dd69ddd
Vu Minh Duc <vu.minh.duc@framgia.com>
2015-06-24 11:11:55 +0700

    edit something send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :user_name => "dev.ducvu@gmail.com",
-    :password => "pa.ana.m",
+    :user_name => "invisible.masks@gmail.com",
+    :password => "thresh256",
     :authentication => "plain",
     :enable_starttls_auto => true
   }
