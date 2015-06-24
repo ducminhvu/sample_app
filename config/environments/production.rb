58b44a4011ffb589bd04744df26494a77eec8d09
Vu Minh Duc <vu.minh.duc@framgia.com>
2015-06-24 11:19:02 +0700

    Edit send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = "sheltered-cliffs-1648.herokuapp.com"
+  host = "blooming-wildwood-9940.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
   
   config.action_mailer.smtp_settings = {
